12b332bf4d2625e546d2a65ddcd12f977c449a40
nickhitt <73009988+nickhitt@users.noreply.github.com>
2022-04-01 20:09:56 +1300

    Beginning data cleaning for Cluster Analysis

--- 1.0 Data Clean.R
+++ 1.0 Data Clean.R
@@ -1,1 +1,13 @@
 ## Data Clean
+
+headers <- names(all_recons[[2]])
+
+spine <- data.frame(all_recons[[2]][[1]])
+for (i in 2:length(all_recons[[2]])){
+  spine[,i] <- all_recons[[2]][[i]]
+}
+
+colnames(spine) <- headers
+
+spine <- spine %>%
+  dplyr::slice(2:length(spine[,1]),)
--- 2.1 EDA - Cluster Analysis.R
+++ 2.1 EDA - Cluster Analysis.R
@@ -1,3 +1,7 @@
 ## EDA - Cluster Analysis
 
 # Data Prep
+spine <- all_recons[[2]][[1]]
+for (i in 1:(length(all_recons[[2]])/4)){
+  
+}
